5d37fedcbf1137ee423bf8e868d68d6937780ad4
Kalu David <kaludavidinyang@gmail.com>
2024-11-14 20:17:44 +0100

    test

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Header } from '../../components/Header';
 import { Button } from '../../components/Button';
 import artist from "/imgs/artist.svg"
@@ -8,6 +8,21 @@ import { Trending } from '../../components/trending/Trending';
 import { ImSearch } from "react-icons/im";
 import { Link } from 'react-router-dom';
 export function Home() {
+
+    useEffect(function () {
+        async function fetchNewReleased() {
+
+            try {
+                const res = await fetch("https://api.deezer.com/chart/116/albums");
+                const data = await res.json();
+                console.log(data)
+            } catch (error) {
+                console.log(error.message)
+            }
+        }
+        fetchNewReleased();
+    }, [])
+
     return (
         <div className='font-Vazirmatn flex flex-col gap-[100px]  px-[20px] pr-[30px]'>
             {/* Hero section */}
